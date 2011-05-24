d3b626c593c96cf1eb1717014929ede53834faa2
Jack Polgar <jack@polgar.id.au>
2011-05-24 20:06:38 +1000

    First commit, nothing working yet..

---
+++ core/load.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Avalon
+ * Copyright (C) 2011 Jack Polgar
+ * 
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD License
+ */
+
+/**
+ * Avalon's Loader class
+ * @package Avalon
+ */
+class Load
+{
+	private $undo = array('my_sql' => 'mysql');
+	private $libs = array();
+	
+	/**
+	 * Library loader
+	 * @param string $class The class name
+	 * @param boolean $init Initialize the class or not
+	 */
+	public static function lib($class, $init = true)
+	{
+		if (isset(static::$libs[$class])) {
+			return static::$libs[$class];
+		}
+		
+		$class_name = ucfirst($class);
+		$file_name = static::lowercase($class);
+		
+		if (file_exists(APPPATH . '/libs/' . $file_name . '.php')) {
+			require_once APPPATH . '/libs/' . $file_name . '.php';
+		} elseif (file_exists(SYSPATH . '/libs/' . $file_name . '.php')) {
+			require_once SYSPATH . '/libs/' . $file_name . '.php';
+		} else {
+			new Error("Loader Error", "Unable to load library '{$class}'", 'HALT');
+		}
+		
+		if ($init) {
+			static::$libs[$class] = new $class_name();
+		} else {
+			static::$libs[$class] = $class_name;
+		}
+		
+		return static::$libs[$class];
+	}
+	
+	private static function lowercase($string) {
+		$string = strtolower(preg_replace('/(?<=[a-z])([A-Z])/', '_' . '\\1', $string));
+		
+		return str_replace(array_keys(static::$undo), array_values(static::$undo), $string);
+	}
+}
