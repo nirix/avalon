e9ed74565426165825d182d9913ffa18881c4132
Jack Polgar <jack@polgar.id.au>
2011-05-25 15:59:37 +1000

    Copyright info added to the Output class

--- libs/output.php
+++ libs/output.php
@@ -1,5 +1,15 @@
 <?php
+/**
+ * Avalon
+ * Copyright (C) 2011 Jack Polgar
+ * 
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD License
+ */
 
+/**
+ * Content output class
+ * @package Avalon
+ */
 class Output
 {
 	private static $body = '';
