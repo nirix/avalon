84b7ce8ccbe27235af23896755b8ccbf06cfd1b2
Jack Polgar <nrx@nirix.net>
2012-01-26 13:14:01 +1100

    Form helper tweaks.

--- helpers/form.php
+++ helpers/form.php
@@ -96,7 +96,7 @@ class Form
 	}
 	
 	/**
-	 * Creates a text input field.
+	 * Creates a select field.
 	 *
 	 * @param string $name
 	 * @param array $attributes
@@ -105,12 +105,20 @@ class Form
 	 */
 	public static function select($name, $values, $attributes = array())
 	{
-		$select = '<select name="' . $name . '"' . (isset($attributes['id']) ? ' id="' . $attributes['id'] . '"' :'') . '>';
+		// Extract the value
+		$value = isset($attributes['value']) ? $attributes['value'] : null;
+		unset($attributes['value']);
 		
-		foreach ($values as $value) {
-			$select .= '<option value="' . $value['value'] . '"' . (@$attributes['value'] == $value['value'] ? ' selected="selected"' :'') . '>' . $value['text'] . '</option>';
+		// Opening tag
+		$select = "<select " . HTML::build_attributes($attributes) . ">";
+		
+		// Options
+		foreach ($values as $value)
+		{
+			$select .= '<option value="' . $value['value'] . '"' . ($value == $value['value'] ? ' selected="selected"' :'') . '>' . $value['label'] . '</option>';
 		}
 		
+		// Closing tags
 		$select .= '</select>';
 		
 		return $select;
@@ -127,24 +135,49 @@ class Form
 	 */
 	public static function input($type, $name, $attributes)
 	{
-		if (isset($attributes['value'])) {
+		// Check if the value is set in the
+		// attributes array
+		if (isset($attributes['value']))
+		{
 			$value = $attributes['value'];
-		} elseif(isset($_POST[$name])) {
+		}
+		// Check if its in the _POST array
+		elseif (isset($_POST[$name]))
+		{
 			$value = $_POST[$name];
-		} else {
+		}
+		// It's nowhere...
+		else
+		{
 			$value = '';
 		}
 		
+		// Merge default attributes with
+		// the specified attributes.
+		$attributes = array_merge(array('type' => $type, 'name' => $name), $attributes);
+		
+		// Text, password and hidden fields
 		if ($type == 'text'
-		or  $type == 'password'
-		or  $type == 'hidden') {
-			return '<input type="' . $type . '" name="' . $name . '" value="' . $value . '"' . (isset($attributes['id']) ? ' id="' . $attributes['id'] . '"' :'') . ' class="' . $type . '">';
-		} elseif($type == 'textarea') {
-			return '<textarea name="' . $name . '"'.(isset($attributes['id']) ? ' id="'.$attributes['id'] . '"' :'') . (isset($attributes['class']) ? ' class="' . $attributes['class'] . '"' :'') . (isset($attributes['cols']) ? ' cols="' . $attributes['cols'] . '"' :'') . (isset($attributes['rows']) ? ' rows="' . $attributes['rows'] . '"' :'') . '>' . $value . '</textarea>';
-		} elseif($type == 'submit') {
-			return '<input type="' . $type . '" value="' . $value . '" class="' . $type . '" />';
-		} elseif($type == 'checkbox') {
-			return '<input type="' . $type . '" name="' . $name . '" value="' . $value . '" class="' . $type . '" ' . (isset($attributes['checked']) && $attributes['checked'] ? 'checked ' :'') . (isset($attributes['id']) ? ' id="' . $attributes['id'] . '"' :'') . '>';
+		or $type == 'password'
+		or $type == 'hidden')
+		{
+			$attributes['value'] = $value;
+			return "<input " . HTML::build_attributes($attributes) . ">";
+		}
+		// Text area
+		elseif ($type == 'textarea')
+		{
+			return '<textarea name="'.$name.'"'.(isset($args['id']) ? ' id="'.$args['id'].'"' :'').(isset($args['class']) ? ' class="'.$args['class'].'"' :'').(isset($args['cols']) ? ' cols="'.$args['cols'].'"' :'').(isset($args['rows']) ? ' rows="'.$args['rows'].'"' :'').'>'.$value.'</textarea>';
+		}
+		// Submit button
+		elseif ($type == 'submit')
+		{
+			return '<input type="'.$type.'" value="'.$value.'" class="'.$type.'" />';
+		}
+		// Checkbox
+		elseif ($type == 'checkbox')
+		{
+			return '<input type="'.$type.'" name="'.$name.'" value="'.$value.'" class="'.$type.'" '.(isset($args['checked']) && $args['checked'] ? 'checked ' :'').(isset($args['id']) ? ' id="'.$args['id'].'"' :'').'/>';
 		}
 	}
 }
