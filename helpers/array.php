e5995a3f4cc40bfc59d10033a34cf600cde11b56
Jack Polgar <nrx@nirix.net>
2012-02-28 22:02:43 +1100

    Added the to_array() function to the array helper.

--- helpers/array.php
+++ helpers/array.php
@@ -36,3 +36,67 @@ function array_remove_keys($array, $keys)
 
 	return $array;
 }
+
+/**
+ * Converts the given data to an array.
+ *
+ * @param mixed $data
+ *
+ * @return array
+ */
+function to_array($data)
+{
+	/*
+	// Check if it's a boolean
+	if (is_bool($data)
+	or is_double($data)   // or a double
+	or is_float($data)    // or a float
+	or is_int($data)      // or an integer
+	or is_long($data)     // or a long integer
+	or is_null($data)     // or if its null
+	or is_numeric($data)  // or some kind of numeric thing
+	or is_string($data))  // or even a regular string
+	{
+		// If so, return it
+		return $data;
+	}
+	// Is it an array itself?
+	// lets try to convert its values...
+	elseif (in_array($data))
+	{
+		// Loop over the data
+		foreach ($data as $key => $val)
+		{
+			// and turn it into an array
+			$data[$key] = to_array($val);
+		}
+		return $data;
+	}
+	*/
+	// Is it an object with a __toArray() method?
+	if (is_object($data) and method_exists($data, '__toArray'))
+	{
+		// Hell yeah, we don't need to do anything.
+		return $data->__toArray();
+	}
+	// Just an object, take its variables!
+	elseif (is_object($data))
+	{
+		// Create an array
+		$array = array();
+
+		// Loop over the classes variables
+		foreach (get_class_vars($data) as $var => $val)
+		{
+			// And steal them! MY PRECIOUS!
+			$array[$var] = $val;
+		}
+
+		// And return the array.
+		return $array;
+	}
+
+	// I have no idea what it is...
+	// just return it...
+	return $data;
+}
