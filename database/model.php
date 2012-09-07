a3993c37ea0d08cf1898e2dd3f84952a03a89a00
Jack Polgar <nrx@nirix.net>
2012-09-08 06:39:31 +1000

    Move opening brackets off of their own line.

--- database/model.php
+++ database/model.php
@@ -50,59 +50,50 @@ class Model
 	protected $_data = array();
 	protected $_is_new = true; // Used to determine if this is a new row or not.
 	protected $errors = array();
-	
+
 	/**
 	 * Used to build to assign the row data to the class as variables.
 	 *
 	 * @param array $data The row data
 	 */
-	public function __construct($data = null, $is_new = true)
-	{
+	public function __construct($data = null, $is_new = true) {
 		$this->_data = $data;
 		$this->_is_new = $is_new;
 
 		// Is there any data?
-		if (is_array($data))
-		{
+		if (is_array($data)) {
 			// If so get the columns and add them to
 			// the properties array
-			foreach (array_keys($data) as $column)
-			{
+			foreach (array_keys($data) as $column) {
 				static::$_properties[] = $column;
 			}
 		}
-		
+
 		// Create filter arrays if they aren't already
-		foreach (array('construct', 'create', 'save') as $filter)
-		{
+		foreach (array('construct', 'create', 'save') as $filter) {
 			// Before filters
-			if (!isset(static::$_filters_before[$filter]))
-			{
+			if (!isset(static::$_filters_before[$filter])) {
 				static::$_filters_before[$filter] = array();
 			}
 
 			// After filters
-			if (!isset(static::$_filters_after[$filter]))
-			{
+			if (!isset(static::$_filters_after[$filter])) {
 				static::$_filters_after[$filter] = array();
 			}
 		}
 
-		if (!in_array('_date_time_convert', static::$_filters_after['construct']))
-		{
+		if (!in_array('_date_time_convert', static::$_filters_after['construct'])) {
 			static::$_filters_after['construct'][] = '_date_time_convert';
 		}
 
-		if (!in_array('_timestamps', static::$_filters_before['create']))
-		{
+		if (!in_array('_timestamps', static::$_filters_before['create'])) {
 			static::$_filters_before['create'][] = '_timestamps';
 		}
 
-		if (!in_array('_timestamps', static::$_filters_before['save']))
-		{
+		if (!in_array('_timestamps', static::$_filters_before['save'])) {
 			static::$_filters_before['save'][] = '_timestamps';
 		}
-		
+
 		// And run the after construct filter array...
 		if (isset(static::$_filters_after['construct'])) {
 			foreach (static::$_filters_after['construct'] as $filter) {
@@ -113,7 +104,7 @@ class Model
 		// Plugin hook
 		FishHook::run('model::__construct', array(get_called_class(), $this, &static::$_properties));
 	}
-	
+
 	/**
 	 * Find the first matching row and returns it.
 	 *
@@ -122,51 +113,44 @@ class Model
 	 *
 	 * @return Object
 	 */
-	public static function find($find, $value = null)
-	{
+	public static function find($find, $value = null) {
 		$select = static::db()->select()->from(static::$_name);
 		if ($value == null) {
 			$select = $select->where(static::$_primary_key, $find)->limit(1)->exec();
 		} else {
 			$select = $select->where($find, $value)->limit(1)->exec();
 		}
-		
-		if ($select->row_count() == 0)
-		{
+
+		if ($select->row_count() == 0) {
 			return false;
 		}
 
 		// Plugin hook
 		FishHook::run('model::find', array(get_called_class(), $find, $value));
-		
+
 		return new static($select->fetch(), false);
 	}
-	
+
 	/**
 	 * Creates a new row or saves the changed properties.
 	 */
-	public function save()
-	{
+	public function save() {
 		$primary_key = static::$_primary_key;
-		
+
 		// Make sure the data is valid..
-		if (!$this->is_valid())
-		{
+		if (!$this->is_valid()) {
 			return false;
 		}
-		
+
 		// Save
-		if ($this->_is_new() === false)
-		{
+		if ($this->_is_new() === false) {
 			// Before save filters
-			if (isset(static::$_filters_before['save']) and is_array(static::$_filters_before['save']))
-			{
-				foreach (static::$_filters_before['save'] as $filter)
-				{
+			if (isset(static::$_filters_before['save']) and is_array(static::$_filters_before['save'])) {
+				foreach (static::$_filters_before['save'] as $filter) {
 					$this->$filter();
 				}
 			}
-			
+
 			// Loop over the properties
 			$data = array();
 			foreach (static::$_properties as $column) {
@@ -176,7 +160,7 @@ class Model
 				}
 			}
 			unset($data[static::$_primary_key]);
-			
+
 			FishHook::run('model::save/save', array(get_called_class(), &$data));
 
 			// Save the row..
@@ -188,20 +172,16 @@ class Model
 		else
 		{
 			// Before create filters
-			if (isset(static::$_filters_before['create']) and is_array(static::$_filters_before['create']))
-			{
-				foreach (static::$_filters_before['create'] as $filter)
-				{
+			if (isset(static::$_filters_before['create']) and is_array(static::$_filters_before['create'])) {
+				foreach (static::$_filters_before['create'] as $filter) {
 					$this->$filter();
 				}
 			}
-			
+
 			// Loop over the properties
 			$data = array();
-			foreach (static::$_properties as $column)
-			{
-				if (isset($this->_data[$column]))
-				{
+			foreach (static::$_properties as $column) {
+				if (isset($this->_data[$column])) {
 					$data[$column] = $this->_data[$column];
 				}
 			}
@@ -211,37 +191,35 @@ class Model
 
 			// Insert the row..
 			static::db()->insert($data)->into(static::$_name)->exec();
-			
+
 			// Set the primary key
 			$this->_data[$primary_key] = static::db()->last_insert_id();
 
 			return true;
 		}
 	}
-	
+
 	/**
 	 * Deletes the row.
 	 */
-	public function delete()
-	{
+	public function delete() {
 		if ($this->_is_new() === false) {
 			return static::db()->delete()->from(static::$_name)->where(static::$_primary_key, $this->_data[static::$_primary_key])->exec();
 		}
 	}
-	
+
 	/**
 	 * Checks if the row is new or not.
 	 *
 	 * @return bool
 	 */
-	public function _is_new($is_new = null)
-	{
+	public function _is_new($is_new = null) {
 		if ($is_new !== null) {
 			$this->_is_new =  $is_new;
 		}
 		return $this->_is_new;
 	}
-	
+
 	/**
 	 * Sets the value of the column(s) to the value(s).
 	 *
@@ -251,8 +229,7 @@ class Model
 	 * @example $model->set(array('col1'=>'val1', 'col2'=>'val2'));
 	 *          $model->set('col1', 'val1');
 	 */
-	public function set($col, $val = null)
-	{
+	public function set($col, $val = null) {
 		if (is_array($col)) {
 			foreach ($col as $var => $val) {
 				$this->set($var, $val);
@@ -260,9 +237,8 @@ class Model
 		} else {
 			$this->_data[$col] = $val;
 			$this->_set_changed($col);
-			
-			if (!in_array($val, static::$_properties))
-			{
+
+			if (!in_array($val, static::$_properties)) {
 				static::$_properties[] = $val;
 			}
 
@@ -270,20 +246,18 @@ class Model
 			FishHook::run('model::set', array(get_called_class(), $col, $val));
 		}
 	}
-	
+
 	/**
 	 * Adds the property to the changed properties array.
 	 *
 	 * @param string $property
 	 */
-	protected function _set_changed($property)
-	{
-		if (in_array($property, static::$_properties) and !in_array($property, $this->_changed_properties))
-		{
+	protected function _set_changed($property) {
+		if (in_array($property, static::$_properties) and !in_array($property, $this->_changed_properties)) {
 			$this->_changed_properties[] = $property;
 		}
 	}
-	
+
 	/**
 	 * Shortcut of the select() function for the database.
 	 *
@@ -291,63 +265,56 @@ class Model
 	 *
 	 * @return object
 	 */
-	public static function select($cols = null)
-	{
+	public static function select($cols = null) {
 		return static::db()->select($cols === null ? static::$_properties : $cols)->from(static::$_name)->_model(static::_class());
 	}
-	
+
 	/**
 	 * Aliases the database's update() method for the current row.
 	 */
-	public function update()
-	{
+	public function update() {
 		return static::db()->update(static::$_name)->where(static::$_primary_key, $this->data[static::$_primary_key]);
 	}
-		
+
 	/**
 	 * Fetches all the rows for the table.
 	 *
 	 * @return array
 	 */
-	public static function fetch_all()
-	{
+	public static function fetch_all() {
 		$rows = array();
 		$fetched = static::db()->select(static::$_properties)->from(static::$_name)->exec()->fetch_all();
-		
+
 		foreach ($fetched as $row) {
 			$rows[] = new static($row);
 			unset($row);
 		}
-		
+
 		return $rows;
 	}
-	
-	public function is_valid()
-	{
+
+	public function is_valid() {
 		// Until the validation stuff is done we will return false,
 		// to work around this each model will have to create its own
 		// is_valid method.
 		return false;
 	}
-	
+
 	/**
 	 * Magical function to load the relationships.
 	 */
-	public function __get($var)
-	{
+	public function __get($var) {
 		// Model data
-		if (in_array($var, static::$_properties))
-		{
+		if (in_array($var, static::$_properties)) {
 			$val = isset($this->_data[$var]) ? $this->_data[$var] : '';
-			
+
 			// Plugin hook
 			FishHook::run('model::__get', array(get_called_class(), $var, $this->_data, &$val));
-			
+
 			return $val;
 		}
 		// Has many
-		else if (is_array(static::$_has_many) and (in_array($var, static::$_has_many) or isset(static::$_has_many[$var])))
-		{
+		else if (is_array(static::$_has_many) and (in_array($var, static::$_has_many) or isset(static::$_has_many[$var]))) {
 			$has_many = array();
 			if (isset(static::$_has_many[$var])) {
 				$has_many = static::$_has_many[$var];
@@ -364,14 +331,13 @@ class Model
 			if (!isset($has_many['column'])) {
 				$has_many['column'] = static::$_primary_key;
 			}
-			
+
 			$model = $has_many['model'];
 			$column = $has_many['column'];
 			return $this->$var = $model::select()->where($has_many['foreign_key'], $this->$column);
 		}
 		// Belongs to
-		else if (is_array(static::$_belongs_to) and (in_array($var, static::$_belongs_to) or isset(static::$_belongs_to[$var])))
-		{
+		else if (is_array(static::$_belongs_to) and (in_array($var, static::$_belongs_to) or isset(static::$_belongs_to[$var]))) {
 			$belongs_to = array();
 			if (isset(static::$_belongs_to[$var])) {
 				$belongs_to = static::$_belongs_to[$var];
@@ -401,14 +367,12 @@ class Model
 			return $val;
 		}
 	}
-	
+
 	/**
 	 * Magical set function to check if the property exists or not.
 	 */
-	public function __set($var, $val)
-	{
-		if (in_array($var, static::$_properties))
-		{
+	public function __set($var, $val) {
+		if (in_array($var, static::$_properties)) {
 			FishHook::run('model::__set', array(get_called_class(), $var, &$val));
 			$this->_data[$var] = $val;
 			$this->_set_changed($var);
@@ -424,19 +388,16 @@ class Model
 	 *
 	 * @return array
 	 */
-	public function __toArray($fields = null)
-	{
+	public function __toArray($fields = null) {
 		// Returns the models data for all fields
-		if ($fields == null)
-		{
+		if ($fields == null) {
 			return $this->_data;
 		}
 		// Return only the fields specified
 		else
 		{
 			$data = array();
-			foreach($fields as $field)
-			{
+			foreach($fields as $field) {
 				$data[$field] = $this->_data[$field];
 			}
 			unset($fields, $field);
@@ -450,35 +411,30 @@ class Model
 	 * @param string $field
 	 * @param string $message
 	 */
-	public function _add_error($field, $message)
-	{
+	public function _add_error($field, $message) {
 		$this->errors[$field] = $message;
 	}
-	
+
 	/**
 	 * Returns the real name of this model class, not the top-most parent.
 	 *
 	 * @return string
 	 */
-	public static function _class()
-	{
+	public static function _class() {
 		return get_class(new static());
 	}
-	
+
 	/**
 	 * Sets the created_at and updated_at fields when saving.
 	 */
-	private function _timestamps()
-	{		
+	private function _timestamps() {
 		// Created at field
-		if ($this->_is_new() and in_array('created_at', static::$_properties) and !isset($this->_data['created_at']))
-		{
+		if ($this->_is_new() and in_array('created_at', static::$_properties) and !isset($this->_data['created_at'])) {
 			$this->_data['created_at'] = "NOW()";
 		}
 
 		// Updated at field
-		if (!$this->_is_new() and in_array('updated_at', static::$_properties))
-		{
+		if (!$this->_is_new() and in_array('updated_at', static::$_properties)) {
 			$this->updated_at = "NOW()";
 		}
 	}
@@ -487,8 +443,7 @@ class Model
 	 * Converts the created_at, updated_at and published_at properties
 	 * to local time from gmt time.
 	 */
-	private function _date_time_convert()
-	{
+	private function _date_time_convert() {
 		foreach (array('created_at', 'updated_at', 'published_at') as $var) {
 			if (!$this->_is_new() and isset($this->_data[$var])) {
 				$this->_data[$var] = Time::gmt_to_local($this->_data[$var]);
@@ -501,8 +456,7 @@ class Model
 	 *
 	 * @return object
 	 */
-	protected static function db()
-	{
+	protected static function db() {
 		return Database::connection(static::$_connection_name);
 	}
 }
