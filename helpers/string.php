7532cc22ba9838fb36614a597b480b13306b03e5
Jack Polgar <nrx@nirix.net>
2012-09-22 04:29:34 +1000

    Added a string helper with strshorten($string, $length) for shortening strings.

---
+++ helpers/string.php
@@ -0,0 +1,39 @@
+<?php
+/*!
+ * Avalon
+ * Copyright (C) 2011-2012 Jack Polgar
+ *
+ * This file is part of Avalon.
+ *
+ * Avalon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; version 3 only.
+ *
+ * Avalon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Avalon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Shortens a string to the set length
+ * and appends '...' to the end.
+ *
+ * @param string  $string
+ * @param integer $length
+ * @param string  $append
+ *
+ * @return string
+ */
+function strshorten($string, $length, $append = '...')
+{
+	// Check if it's longer than the length
+	if (isset($string[$length-1])) {
+		return substr($string, 0, $length) . '...';
+	} else {
+		return $string;
+	}
+}
