e127e4382e607e021147913fb053d79382b56314
Jack Polgar <nrx@nirix.net>
2012-11-19 11:57:34 +1100

    HTTP files updated to use new code formatting rules.

--- http/request.php
+++ http/request.php
@@ -28,225 +28,225 @@ namespace avalon\http;
  */
 class Request
 {
-	private static $entry_file;
-	private static $path_info;
-	private static $base_url;
-	private static $url;
-	private static $segments;
-	private static $requested_with;
-	public static $request;
-	public static $method;
-	public static $post;
-	
-	/**
-	 * Processes the request and gets the URL,
-	 * request method, request type, and so on.
-	 */
-	public static function process()
-	{
-		static::$url = trim(static::_get_path_info(), '/');
-		static::$segments = explode('/', trim(static::$url, '/'));
-		static::$requested_with = @$_SERVER['HTTP_X_REQUESTED_WITH'];
-		static::$request = $_REQUEST;
-		static::$method = strtolower($_SERVER['REQUEST_METHOD']);
-		static::$post = $_POST;
-	}
-	
-	/**
-	 * Determines the base URL of the app.
-	 *
-	 * @return string
-	 */
-	public static function base()
-	{
-		return str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']) . (func_num_args() > 0 ? trim(implode('/' , func_get_args()), '/') : '');
-	}
-	
-	/**
-	 * Redirects to the specified URL.
-	 *
-	 * @param string $url
-	 */
-	public static function redirect($url)
-	{
-		header("Location: " . $url);
-		exit;
-	}
-	
-	/**
-	 * Returns the requested URL.
-	 *
-	 * @return string
-	 */
-	public static function url()
-	{
-		return '/' . static::$url;
-	}
-	
-	/**
-	 * Returns the full URI of the request.
-	 *
-	 * @return string
-	 */
-	public static function full_uri()
-	{
-		return static::base(trim(static::url(), '/'));
-	}
-	
-	/**
-	 * Checks of the URI matches the specified URI.
-	 *
-	 * @param string $uri
-	 *
-	 * @return bool
-	 */
-	public static function matches($uri)
-	{
-		return trim($uri, '/') == trim(implode('/', self::$segments), '/');
-	}
-	
-	/**
-	 * Returns the segment at the specified index.
-	 *
-	 * @param integer $num
-	 *
-	 * @return string
-	 */
-	public static function seg($num)
-	{
-		return @static::$segments[$num];
-	}
-	
-	/**
-	 * Checks if the request was made via Ajax.
-	 *
-	 * @return bool
-	 */
-	public static function is_ajax()
-	{
-		return strtolower(static::$requested_with) == 'xmlhttprequest';
-	}
-	
-	/**
-	 * Gets the base URL
-	 *
-	 * @return string
-	 */
-	private static function _get_base_url()
-	{
-		static::$base_url = rtrim(dirname(static::_get_script_url()), '\\/');
-		return static::$base_url;
-	}
-	
-	/**
-	 * Determines the path info.
-	 *
-	 * @return string
-	 */
-	private static function _get_path_info()
-	{
-		if (static::$path_info === null) {
-			$path_info = static::_get_request_url();
+    private static $entry_file;
+    private static $path_info;
+    private static $base_url;
+    private static $url;
+    private static $segments;
+    private static $requested_with;
+    public static $request;
+    public static $method;
+    public static $post;
 
-			if (($pos = strpos($path_info, '?')) !== false) {
-			   $path_info = substr($path_info, 0, $pos);
-			}
-			$path_info = urldecode($path_info);
+    /**
+     * Processes the request and gets the URL,
+     * request method, request type, and so on.
+     */
+    public static function process()
+    {
+        static::$url = trim(static::_get_path_info(), '/');
+        static::$segments = explode('/', trim(static::$url, '/'));
+        static::$requested_with = @$_SERVER['HTTP_X_REQUESTED_WITH'];
+        static::$request = $_REQUEST;
+        static::$method = strtolower($_SERVER['REQUEST_METHOD']);
+        static::$post = $_POST;
+    }
 
-			$script_url = static::_get_script_url();
-			$base_url = static::_get_base_url();
-			if (strpos($path_info, $script_url) === 0) {
-				$path_info = substr($path_info, strlen($script_url));
-			}
-			elseif ($base_url === '' || strpos($path_info, $base_url) === 0) {
-				$path_info = substr($path_info, strlen($base_url));
-			}
-			elseif (strpos($_SERVER['PHP_SELF'],$script_url) === 0) {
-				$path_info = substr($_SERVER['PHP_SELF'], strlen($script_url));
-			} else {
-				throw new Exception("Unable to determin path info.");
-			}
-			
-			static::$path_info = trim($path_info, '/');
-		}
-		
-		return static::$path_info;
-	}
-	
-	/**
-	 * Determines the script URL.
-	 *
-	 * @return string
-	 */
-	private static function _get_script_url()
-	{
-		if (static::$entry_file === null) {
-			$file_name = basename($_SERVER['SCRIPT_FILENAME']);
-			
-			// script_name
-			if (basename($_SERVER['SCRIPT_NAME']) === $file_name) {
-				static::$entry_file = $_SERVER['SCRIPT_NAME'];
-			}
-			// php_self
-			elseif (basename($_SERVER['PHP_SELF']) === $file_name) {
-				static::$entry_file = $_SERVER['PHP_SELF'];
-			}
-			// orig_script_name
-			elseif (isset($_SERVER['ORIG_SCRIPT_NAME']) && basename($_SERVER['ORIG_SCRIPT_NAME']) === $file_name)
-			{
-				static::$entry_file = $_SERVER['ORIG_SCRIPT_NAME'];
-			}
-			// more php_self
-			elseif (($pos=strpos($_SERVER['PHP_SELF'], '/' . $file_name))!==false)
-			{
-				static::$entry_file = substr($_SERVER['SCRIPT_NAME'], 0, $pos) . '/' . $file_name;
-			}
-			// document_root
-			elseif (isset($_SERVER['DOCUMENT_ROOT']) && strpos($_SERVER['SCRIPT_FILENAME'], $_SERVER['DOCUMENT_ROOT']) === 0)
-			{
-				static::$entry_file = str_replace('\\', '/', str_replace($_SERVER['DOCUMENT_ROOT'], '', $_SERVER['SCRIPT_FILENAME']));
-			}
-			// /wrists
-			else {
-				throw new Exception("Unable to determin entry file.");
-			}
-		}
-		
-		return static::$entry_file;
-	}
-	
-	/**
-	 * Determines the requested URL.
-	 *
-	 * @return string
-	 */
-	private static function _get_request_url()
-	{
-		if (static::$url === null) {
-			// IIS
-			if (isset($_SERVER['HTTP_X_REWRITE_URL'])) {
-				static::$url = $_SERVER['HTTP_X_REWRITE_URL'];
-			} elseif (isset($_SERVER['REQUEST_URI'])) {
-				static::$url = $_SERVER['REQUEST_URI'];
-				if(isset($_SERVER['HTTP_HOST'])) {
-					if (strpos(static::$url, $_SERVER['HTTP_HOST']) !== false) {
-						static::$url = preg_replace('/^\w+:\/\/[^\/]+/', '', static::$url);
-					}
-				} else {
-					static::$url = preg_replace('/^(http|https):\/\/[^\/]+/i', '', static::$url);
-				}
-			}
-			// IIS 5.0 CGI
-			elseif (isset($_SERVER['ORIG_PATH_INFO'])) {
-				static::$url = $_SERVER['ORIG_PATH_INFO'];
-				if(!empty($_SERVER['QUERY_STRING'])) {
-					static::$url .= '?' . $_SERVER['QUERY_STRING'];
-				}
-			} else {
-				throw new Exception("Unable to determin URI.");
-			}
-		}
-		
-		return static::$url;
-	}
+    /**
+     * Determines the base URL of the app.
+     *
+     * @return string
+     */
+    public static function base()
+    {
+        return str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']) . (func_num_args() > 0 ? trim(implode('/' , func_get_args()), '/') : '');
+    }
+
+    /**
+     * Redirects to the specified URL.
+     *
+     * @param string $url
+     */
+    public static function redirect($url)
+    {
+        header("Location: " . $url);
+        exit;
+    }
+
+    /**
+     * Returns the requested URL.
+     *
+     * @return string
+     */
+    public static function url()
+    {
+        return '/' . static::$url;
+    }
+
+    /**
+     * Returns the full URI of the request.
+     *
+     * @return string
+     */
+    public static function full_uri()
+    {
+        return static::base(trim(static::url(), '/'));
+    }
+
+    /**
+     * Checks of the URI matches the specified URI.
+     *
+     * @param string $uri
+     *
+     * @return bool
+     */
+    public static function matches($uri)
+    {
+        return trim($uri, '/') == trim(implode('/', self::$segments), '/');
+    }
+
+    /**
+     * Returns the segment at the specified index.
+     *
+     * @param integer $num
+     *
+     * @return string
+     */
+    public static function seg($num)
+    {
+        return @static::$segments[$num];
+    }
+
+    /**
+     * Checks if the request was made via Ajax.
+     *
+     * @return bool
+     */
+    public static function is_ajax()
+    {
+        return strtolower(static::$requested_with) == 'xmlhttprequest';
+    }
+
+    /**
+     * Gets the base URL
+     *
+     * @return string
+     */
+    private static function _get_base_url()
+    {
+        static::$base_url = rtrim(dirname(static::_get_script_url()), '\\/');
+        return static::$base_url;
+    }
+
+    /**
+     * Determines the path info.
+     *
+     * @return string
+     */
+    private static function _get_path_info()
+    {
+        if (static::$path_info === null) {
+            $path_info = static::_get_request_url();
+
+            if (($pos = strpos($path_info, '?')) !== false) {
+               $path_info = substr($path_info, 0, $pos);
+            }
+            $path_info = urldecode($path_info);
+
+            $script_url = static::_get_script_url();
+            $base_url = static::_get_base_url();
+            if (strpos($path_info, $script_url) === 0) {
+                $path_info = substr($path_info, strlen($script_url));
+            }
+            elseif ($base_url === '' || strpos($path_info, $base_url) === 0) {
+                $path_info = substr($path_info, strlen($base_url));
+            }
+            elseif (strpos($_SERVER['PHP_SELF'],$script_url) === 0) {
+                $path_info = substr($_SERVER['PHP_SELF'], strlen($script_url));
+            } else {
+                throw new Exception("Unable to determin path info.");
+            }
+
+            static::$path_info = trim($path_info, '/');
+        }
+
+        return static::$path_info;
+    }
+
+    /**
+     * Determines the script URL.
+     *
+     * @return string
+     */
+    private static function _get_script_url()
+    {
+        if (static::$entry_file === null) {
+            $file_name = basename($_SERVER['SCRIPT_FILENAME']);
+
+            // script_name
+            if (basename($_SERVER['SCRIPT_NAME']) === $file_name) {
+                static::$entry_file = $_SERVER['SCRIPT_NAME'];
+            }
+            // php_self
+            elseif (basename($_SERVER['PHP_SELF']) === $file_name) {
+                static::$entry_file = $_SERVER['PHP_SELF'];
+            }
+            // orig_script_name
+            elseif (isset($_SERVER['ORIG_SCRIPT_NAME']) && basename($_SERVER['ORIG_SCRIPT_NAME']) === $file_name)
+            {
+                static::$entry_file = $_SERVER['ORIG_SCRIPT_NAME'];
+            }
+            // more php_self
+            elseif (($pos=strpos($_SERVER['PHP_SELF'], '/' . $file_name))!==false)
+            {
+                static::$entry_file = substr($_SERVER['SCRIPT_NAME'], 0, $pos) . '/' . $file_name;
+            }
+            // document_root
+            elseif (isset($_SERVER['DOCUMENT_ROOT']) && strpos($_SERVER['SCRIPT_FILENAME'], $_SERVER['DOCUMENT_ROOT']) === 0)
+            {
+                static::$entry_file = str_replace('\\', '/', str_replace($_SERVER['DOCUMENT_ROOT'], '', $_SERVER['SCRIPT_FILENAME']));
+            }
+            // /wrists
+            else {
+                throw new Exception("Unable to determin entry file.");
+            }
+        }
+
+        return static::$entry_file;
+    }
+
+    /**
+     * Determines the requested URL.
+     *
+     * @return string
+     */
+    private static function _get_request_url()
+    {
+        if (static::$url === null) {
+            // IIS
+            if (isset($_SERVER['HTTP_X_REWRITE_URL'])) {
+                static::$url = $_SERVER['HTTP_X_REWRITE_URL'];
+            } elseif (isset($_SERVER['REQUEST_URI'])) {
+                static::$url = $_SERVER['REQUEST_URI'];
+                if(isset($_SERVER['HTTP_HOST'])) {
+                    if (strpos(static::$url, $_SERVER['HTTP_HOST']) !== false) {
+                        static::$url = preg_replace('/^\w+:\/\/[^\/]+/', '', static::$url);
+                    }
+                } else {
+                    static::$url = preg_replace('/^(http|https):\/\/[^\/]+/i', '', static::$url);
+                }
+            }
+            // IIS 5.0 CGI
+            elseif (isset($_SERVER['ORIG_PATH_INFO'])) {
+                static::$url = $_SERVER['ORIG_PATH_INFO'];
+                if(!empty($_SERVER['QUERY_STRING'])) {
+                    static::$url .= '?' . $_SERVER['QUERY_STRING'];
+                }
+            } else {
+                throw new Exception("Unable to determin URI.");
+            }
+        }
+
+        return static::$url;
+    }
 }
--- http/router.php
+++ http/router.php
@@ -28,100 +28,100 @@ namespace avalon\http;
  */
 class Router
 {
-	public static $namespace;
-	public static $controller;
-	public static $method;
-	public static $params = array();
-	public static $args = array();
-	public static $extension;
-	public static $error404;
-	private static $routes = array();
+    public static $namespace;
+    public static $controller;
+    public static $method;
+    public static $params = array();
+    public static $args = array();
+    public static $extension;
+    public static $error404;
+    private static $routes = array();
 
-	// Router stuff
-	public static $extensions = array('.json', '.xml', '.rss');
-	
-	/**
-	 * Matche the request to a route and get the controller, method and arguments.
-	 * @param string $request The request.
-	 * @return boolean
-	 */
-	public static function process($request)
-	{
-		// Are we on the front page?
-		if ($request == '/') {
-			static::set_request(static::$routes['root']);
-			return true;
-		}
-		
-		// Check if we have an exact match
-		if (isset(static::$routes[$request])) {
-			static::set_request(static::$routes[$request]);
-			return true;
-		}
-		
-		// Loop through routes and find a regex match
-		foreach (static::$routes as $route => $args) {
-			$route = '#^' . $route . '(?<extension>' . implode('|', static::$extensions) . ')?$#';
-			
-			if (preg_match($route, $request, $params)) {
-				unset($params[0]);
-				$args['params'] = array_merge($args['params'], $params);
-				$args['value'] = preg_replace($route, $args['value'], $request);
-				$args['extension'] = isset($params['extension']) ? $params['extension'] : null;
-				static::set_request($args);
-				return true;
-			}
-		}
-		
-		// No match, error controller, make it so.
-		static::set_request(array('value' => 'Error::404', 'params' => array()));
-		return false;
-	}
+    // Router stuff
+    public static $extensions = array('.json', '.xml', '.rss');
 
-	/**
-	 * Add a route.
-	 * @param string $route The route to match to.
-	 * @param string $value The controller and method to route to.
-	 * @param array $params Parameters to be passed to the controller method.
-	 */
-	public static function add($route, $value, $params = array())
-	{
-		if (!isset(static::$routes[$route])) {
-			static::$routes[$route] = array(
-				'template' => $route,
-				'value' => $value,
-				'params' => $params
-			);
-		}
-	}
-	
-	/**
-	 * Private function to set the routed controller, method, parameters and method arguments.
-	 * @param array $route The route array.
-	 */
-	private static function set_request($route)
-	{
-		// Seperate the namespace, controller, method and arguments
-		$bits = explode('::', $route['value']);
-		$method_bits = explode('/', end($bits));
+    /**
+     * Matche the request to a route and get the controller, method and arguments.
+     * @param string $request The request.
+     * @return boolean
+     */
+    public static function process($request)
+    {
+        // Are we on the front page?
+        if ($request == '/') {
+            static::set_request(static::$routes['root']);
+            return true;
+        }
 
-		static::$namespace = ($ns = implode('::', array_slice($bits, 0, -2)) and !empty($ns)) ? $ns : null;
-		static::$controller = $bits[count($bits) - 2];
-		static::$method = $method_bits[0];
-		static::$args = (isset($method_bits[1])) ? explode(',', $method_bits[1]) : array();
-		static::$extension = isset($route['extension']) ? $route['extension'] : null;
-		static::$params = $route['params'];
+        // Check if we have an exact match
+        if (isset(static::$routes[$request])) {
+            static::set_request(static::$routes[$request]);
+            return true;
+        }
 
-		unset($bits, $method_bits, $ns);
-	}
+        // Loop through routes and find a regex match
+        foreach (static::$routes as $route => $args) {
+            $route = '#^' . $route . '(?<extension>' . implode('|', static::$extensions) . ')?$#';
 
-	/**
-	 * Returns the namespace in the form of a directory path.
-	 *
-	 * @return string
-	 */
-	public static function namespace_path()
-	{
-		return str_replace('::', '/', static::$namespace) . '/';
-	}
+            if (preg_match($route, $request, $params)) {
+                unset($params[0]);
+                $args['params'] = array_merge($args['params'], $params);
+                $args['value'] = preg_replace($route, $args['value'], $request);
+                $args['extension'] = isset($params['extension']) ? $params['extension'] : null;
+                static::set_request($args);
+                return true;
+            }
+        }
+
+        // No match, error controller, make it so.
+        static::set_request(array('value' => 'Error::404', 'params' => array()));
+        return false;
+    }
+
+    /**
+     * Add a route.
+     * @param string $route The route to match to.
+     * @param string $value The controller and method to route to.
+     * @param array $params Parameters to be passed to the controller method.
+     */
+    public static function add($route, $value, $params = array())
+    {
+        if (!isset(static::$routes[$route])) {
+            static::$routes[$route] = array(
+                'template' => $route,
+                'value' => $value,
+                'params' => $params
+            );
+        }
+    }
+
+    /**
+     * Private function to set the routed controller, method, parameters and method arguments.
+     * @param array $route The route array.
+     */
+    private static function set_request($route)
+    {
+        // Seperate the namespace, controller, method and arguments
+        $bits = explode('::', $route['value']);
+        $method_bits = explode('/', end($bits));
+
+        static::$namespace = ($ns = implode('::', array_slice($bits, 0, -2)) and !empty($ns)) ? $ns : null;
+        static::$controller = $bits[count($bits) - 2];
+        static::$method = $method_bits[0];
+        static::$args = (isset($method_bits[1])) ? explode(',', $method_bits[1]) : array();
+        static::$extension = isset($route['extension']) ? $route['extension'] : null;
+        static::$params = $route['params'];
+
+        unset($bits, $method_bits, $ns);
+    }
+
+    /**
+     * Returns the namespace in the form of a directory path.
+     *
+     * @return string
+     */
+    public static function namespace_path()
+    {
+        return str_replace('::', '/', static::$namespace) . '/';
+    }
 }
