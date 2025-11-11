1f3f96f9476c9ed529347a0df435010d4084067c
Jack Polgar <hello@jp.id.au>
2025-11-11 23:34:36 +1100

    Add more http status codes

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -32,6 +32,20 @@ class Response
     public const HTTP_MOVED_PERMANENTLY = 301;
     public const HTTP_FOUND = 302;
     public const HTTP_UNPROCESSABLE_ENTITY = 422;
+    public const HTTP_BAD_REQUEST = 400;
+    public const HTTP_UNAUTHORIZED = 401;
+    public const HTTP_FORBIDDEN = 403;
+    public const HTTP_INTERNAL_SERVER_ERROR = 500;
+    public const HTTP_NOT_IMPLEMENTED = 501;
+    public const HTTP_BAD_GATEWAY = 502;
+    public const HTTP_SERVICE_UNAVAILABLE = 503;
+    public const HTTP_GATEWAY_TIMEOUT = 504;
+    public const HTTP_VERSION_NOT_SUPPORTED = 505;
+    public const HTTP_VARIANT_ALSO_NEGOTIATES = 506;
+    public const HTTP_INSUFFICIENT_STORAGE = 507;
+    public const HTTP_LOOP_DETECTED = 508;
+    public const HTTP_NOT_EXTENDED = 510;
+    public const HTTP_NETWORK_AUTHENTICATION_REQUIRED = 511;
 
     public const STATUS_TEXT = [
         Response::HTTP_OK => 'OK',
@@ -39,6 +53,20 @@ class Response
         Response::HTTP_MOVED_PERMANENTLY => 'Moved Permanently',
         Response::HTTP_FOUND => 'Found',
         Response::HTTP_UNPROCESSABLE_ENTITY => 'Unprocessable Entity',
+        Response::HTTP_BAD_REQUEST => 'Bad Request',
+        Response::HTTP_UNAUTHORIZED => 'Unauthorized',
+        Response::HTTP_FORBIDDEN => 'Forbidden',
+        Response::HTTP_INTERNAL_SERVER_ERROR => 'Internal Server Error',
+        Response::HTTP_NOT_IMPLEMENTED => 'Not Implemented',
+        Response::HTTP_BAD_GATEWAY => 'Bad Gateway',
+        Response::HTTP_SERVICE_UNAVAILABLE => 'Service Unavailable',
+        Response::HTTP_GATEWAY_TIMEOUT => 'Gateway Timeout',
+        Response::HTTP_VERSION_NOT_SUPPORTED => 'HTTP Version Not Supported',
+        Response::HTTP_VARIANT_ALSO_NEGOTIATES => 'Variant Also Negotiates',
+        Response::HTTP_INSUFFICIENT_STORAGE => 'Insufficient Storage',
+        Response::HTTP_LOOP_DETECTED => 'Loop Detected',
+        Response::HTTP_NOT_EXTENDED => 'Not Extended',
+        Response::HTTP_NETWORK_AUTHENTICATION_REQUIRED => 'Network Authentication Required',
     ];
 
     public function __construct(
